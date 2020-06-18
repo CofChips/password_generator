e971982dcef5dfdd40bd05043bef1850c554b6ce
CofChips <qiwei.mod@gmail.com>
2020-06-17 23:46:11 -0700

    draft logic for assigning random characters

--- script.js
+++ script.js
@@ -48,21 +48,53 @@ function writePassword() {
 console.log("test");
 
   var password = generatePassword();
+  // var password = "test"
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
   function generatePassword() {
     console.log("test2");
+
+    // informs user that criteria is coming up
     alert("Please enter password criteria on the subsequent screens");
 
+    // password length selection
 while(isNaN(passLength) || passLength < 8 || passLength > 128) {
     passLength = parseInt(prompt("Your password must be 8 - 128 characters in length.  How many characters would you like your password to be (please enter a number from 8 - 128)?")); 
 }
 
+// tells us which criteria is needed
+do {
+  alert("Please select from the following criteria (you must select at least one).")
+  lowercase.isNeeded = confirm("Include lowercase letters?");
+  uppercase.isNeeded = confirm("Include uppercase letters?");
+  numeric.isNeeded = confirm("Include numbers?");
+  specChar.isNeeded = confirm("Include special characters?");
+  
+  }
+  // user must choose at least one criteria 
+  while(lowercase.isNeeded === false && uppercase.isNeeded === false && numeric.isNeeded === false && specChar.isNeeded === false);
+
 for (var i = 0; i < passLength; i++) {
-  var num = Math.floor(Math.random() * 5) + 1;
+  var num = Math.floor(Math.random() * 4) + 1;
   passDraft.push(num);
+}
+
+for (var i = 0; i < passLength; i++) {
+  if (passDraft[i] === 1) {
+      lowercase.letter();
+  }
+  else if (passDraft[i] === 2) {
+      uppercase.letter();
+  }
+  else if (passDraft[i] === 3) {
+      numeric.number();
+  }
+  else if (passDraft[i] === 4) {
+      special.char();
+  }
+
 }
 }
 }
