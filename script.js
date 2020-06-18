508a1e77a4403cdca87a38ca613ed225a83a4f79
CofChips <qiwei.mod@gmail.com>
2020-06-17 20:23:01 -0700

    added initial var list

--- script.js
+++ script.js
@@ -8,7 +8,35 @@ function writePassword() {
 
   passwordText.value = password;
 
+  function generatePassword() {
+    return "test";
+  }
+  alert("Please enter password criteria on the subsequent screens")
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+var lowercaseLetters = "abcdefghijklmnopqrstuvwxyz"
+var updercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var numbers = [1,2,3,4,5,6,7,8,9,0]
+var specChar = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+
+var total = "";
+var passDraft = [];
+var userEntry; 
+var passLength;
+var lowercase = {
+      letter : [],
+      isNeeded: true,
+}
+
+var uppercase = {
+      letter : [],
+      isNeeded: true,
+}
+
+var numeric = {
+      number : [],
+      isNeeded : true, 
+}
